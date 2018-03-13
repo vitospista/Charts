885a2b21149f929a05cce8d5e57d5add16ae6d01
Istvan Vitos <vitospista@gmail.com>
2018-03-13 20:20:51 +0100

    added options to horizontal chart

--- js/script.js
+++ js/script.js
@@ -65,164 +65,7 @@
 	var path2 = Path(310, 10, 40, 100, 'yellow');
 })()
 
-function drawLine(ctx, startX, startY, endX, endY, color){
-	ctx.save();
-	ctx.strokeStyle = color;
-	ctx.beginPath();
-	ctx.moveTo(startX, startY);
-	ctx.lineTo(endX, endY);
-	ctx.stroke();
-	ctx.restore();
-}
- 
-function drawBar(ctx, upperLeftCornerX, upperLeftCornerY, width, height, color){
-	ctx.save();
-	ctx.fillStyle = color;
-	ctx.fillRect(upperLeftCornerX, upperLeftCornerY, width, height);
-	ctx.restore();
-}
-
-function writeText(ctx, color, font, value, x, y){
-	ctx.save();
-	ctx.fillStyle = color;
-	ctx.textBaseline = 'bottom';
-	ctx.font = font;
-	ctx.fillText(value, x, y);
-	ctx.restore();
-}
-
-function drawArc(ctx, centerX, centerY, radius, currentAngle, sliceAngle, color){
-	ctx.beginPath();
-	ctx.arc(centerX, centerY, radius, currentAngle, currentAngle + sliceAngle);	
-	ctx.lineTo(centerX, centerY);
-	ctx.fillStyle = color;
-	ctx.fill();
-}
-
-function constructor(options){
-	this.options = options;
-	this.canvas = options.canvas;
-	this.ctx = this.canvas.getContext('2d');
-	this.colors = options.colors;
-}
-
-function StackedChart(options){
-	constructor.call(this, options);
-
-	this.draw = function(data){
-		let padding = this.options.padding;
-		let barWidth = this.options.barWidth;
-		let spacing = this.options.spacing;
-		let numberOfBars = Object.keys(data).length;
-		this.canvas.width = padding * 2 + numberOfBars*barWidth + (numberOfBars-1)*spacing;
-		let drawingHeight = this.canvas.height - padding * 2;		
-		let currentX = padding;
-		
-		//drawing the bars
-		for (let doc in data){
-			let val = data[doc];
-			let total = Object.values(val).reduce((a,b)=>a+b);					
-			let currentY = padding;
-			
-			for (let result of ['Success', 'Complications', 'Failure']){
-				let barHeight;
-				if (result!=='Failure')
-					barHeight = Math.round( drawingHeight * val[result]/total);
-				else
-					barHeight = drawingHeight - currentY + padding;
-				
-				drawBar(this.ctx, currentX, currentY, barWidth, barHeight, this.colors[result]);				
-				currentY += barHeight;
-			}	
-			currentX += barWidth + spacing;
-		}
-		//drawing the grid lines
-		let scaleValue = 0;
-		while (scaleValue <= 100){
-				var gridY = drawingHeight * (1 - scaleValue/100) + this.options.padding;
-				drawLine(this.ctx, 0, gridY, this.canvas.width, gridY, this.options.gridColor);
-
-				//writing grid markers
-				writeText(this.ctx, this.options.gridColor, this.options.font, scaleValue, 0, gridY)
-				writeText(this.ctx, this.options.gridColor, this.options.font, scaleValue, this.canvas.width-20, gridY)
-				scaleValue+=this.options.gridScale;
-		}
-	}
-}
-
-
-function PieChart(options){	
-	constructor.call(this, options);
-		
-	this.draw = function(data){
-		this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-		let total = Object.values(data).reduce((a,b)=>a+b);
-		let currentAngle = -0.5 * Math.PI;
-		let centerX = (this.canvas.width-this.options.padding*2)/4;
-		let centerY = this.canvas.height/2;
-		let colorIndex = 0;
-		let legendX = this.canvas.width/2 + this.options.padding;
-		let legendY = this.options.padding*2;
-		let dataCount = Object.keys(data).length;
-		let legendSpacing = Math.min(20, ((this.canvas.height-this.options.padding*2)-dataCount*20)/(dataCount-1));
-		
-		for (let doc in data) {
-			let percentage = data[doc] / total;
-			let sliceAngle = (percentage) * 2 * Math.PI;
-			let currentColor = this.colors[colorIndex++%this.colors.length];
-			drawArc(this.ctx, centerX, centerY, centerY-this.options.padding, currentAngle, sliceAngle, currentColor);
-			currentAngle += sliceAngle;
-			
-			let text = doc + ' ' + Math.round(percentage * 100) + '%';
-			writeText(this.ctx, currentColor, this.options.font, text, legendX, legendY);
-			legendY += legendSpacing + 20;
-		}
-	}
-}
-
-function HorizontalChart(options){
-	constructor.call(this, options);	
-
-	this.draw = function(data){
-		let padding = this.options.padding;
-		let spacing = this.options.spacing;
-		let numberOfBars = Object.keys(data).length;				
-		let currentY = padding;
-		let drawingWidth = this.canvas.width - padding*2;
-		let drawingHeight = (this.canvas.height)*.80 - padding*2;
-		let max = Math.max.apply(null, Object.values(data));
-		let colorIndex = 0;
-		let barHeight = (drawingHeight - (numberOfBars-1)*spacing) / numberOfBars;
-		let legendX = padding;
-		
-		for (let categ in data){
-			let val = data[categ];
-			let barWidth = Math.round( drawingWidth * val/max);
-			let currentColor = this.colors[colorIndex++%this.colors.length];
-			drawBar(this.ctx, padding, currentY, barWidth, barHeight, currentColor);	
-			currentY += barHeight+ spacing;	
-			
-			let text = categ;
-			writeText(this.ctx, currentColor, this.options.legendFont, text, legendX, this.canvas.height*.9);
-			legendX += 150;
-		}	
-		
-		//drawing the grid lines
-		let scaleValue = 0;
-		let gridScale = Math.max(1, max/10);
-		while (scaleValue <= max){
-			var gridX = drawingWidth * (scaleValue/max) + this.options.padding;
-			drawLine(this.ctx, gridX, 0, gridX, drawingHeight+2*padding, this.options.gridColor);
-
-			//writing grid markers
-			writeText(this.ctx, this.options.gridColor, this.options.gridFont, scaleValue, gridX+5, 15)
-			writeText(this.ctx, this.options.gridColor, this.options.gridFont, scaleValue, gridX+5, drawingHeight+2*padding-5)
-			scaleValue+=gridScale;
-		}
-	}
-}
-
-var stackedChart = new StackedChart(
+const stackedChart = new StackedChart(
 	{
 		canvas: canvas2,       
 		padding: 20,
@@ -241,7 +84,7 @@ var stackedChart = new StackedChart(
 stackedChart.draw(data.outcome);
 
 
-var pieChart = new PieChart(
+const pieChart = new PieChart(
 	{
 		canvas: canvas3,    
 		padding: 20,	
@@ -249,8 +92,8 @@ var pieChart = new PieChart(
 		colors: ['#fe4e00', '#fd7690', '#ceea8a', '#ffae03', '#aae4b7', '#f0caca', '#d1c4a4', '#66cdaa', '#66cc66', '#ffd319']
 	}
 );
-
-var select = document.getElementById("dropCategories");
+const select = document.getElementById("dropCategories");
+//create buttons for DropDown
 for(let categ in data.operativity) {
 	let btn = document.createElement('button');
 	btn.type = 'button';
@@ -260,8 +103,48 @@ for(let categ in data.operativity) {
 	btn.addEventListener('click', ()=> pieChart.draw(data.operativity[categ]));
 	select.appendChild(btn);
 }
+select.firstElementChild.click();
 
-select.children[0].click();
+
+const horizontalChart = new HorizontalChart(
+	{
+		canvas: canvas4,       
+		padding: 20,
+		spacing: 10,
+		gridColor: 'lightgrey',
+		gridFont: 'bold 10px Arial',
+		legendFont: '20px Monospace',
+		colors: ['#fe4e00', '#fd7690', '#ceea8a', '#ffae03', '#aae4b7', '#f0caca', '#d1c4a4', '#66cdaa', '#66cc66', '#ffd319']
+	}
+);
+const btnYears = document.getElementById('btnYears');
+const chkOutcomes = document.getElementById('chkOutcomes');
+let selectedYearButton = btnYears.firstElementChild;
+let selectedOutcomes = ['Success'];
+refreshSurgeryData();
+
+btnYears.addEventListener('click', function(e){
+	//console.log(e.target);
+	if (e.target !== selectedYearButton){
+		selectedYearButton.classList.remove('active');
+		e.target.classList.add('active');
+		selectedYearButton = e.target;
+		refreshSurgeryData();	
+	}
+});
+
+chkOutcomes.addEventListener('click', function(e){	
+	if (!e.target.type)
+		return;
+	
+	let index = selectedOutcomes.indexOf(e.target.value);
+	if (index === -1){
+		selectedOutcomes.push(e.target.value);
+	} else {
+		selectedOutcomes.splice(index, 1);
+	}
+	refreshSurgeryData();
+})
 
 function filterSurgery(year, outcomes){
 	let result = {};
@@ -279,21 +162,10 @@ function filterSurgery(year, outcomes){
 	return result;
 }
 
-var horizontalChart = new HorizontalChart(
-	{
-		canvas: canvas4,       
-		padding: 20,
-		spacing: 10,
-		gridColor: 'lightgrey',
-		gridFont: 'bold 10px Arial',
-		legendFont: '14px Monospace',
-		colors: ['#fe4e00', '#fd7690', '#ceea8a', '#ffae03', '#aae4b7', '#f0caca', '#d1c4a4', '#66cdaa', '#66cc66', '#ffd319']
-	}
-);
-let testData = filterSurgery(2016, ['Success']);
-horizontalChart.draw(testData);
-
-
+function refreshSurgeryData(){
+	let yearData = filterSurgery(selectedYearButton.value, selectedOutcomes);
+	horizontalChart.draw(yearData);	
+}
 
 
 
